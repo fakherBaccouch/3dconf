4d27b99d37a0315ea02c37f8affdb8de12c1f21c
mohamed.b <bakouchfakher@gmail.com>
2021-11-24 22:05:47 +0100

    first

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
 import React from "react"
-import leather from "./images/leather.png"
+import leather from "./images/leather.svg"
 import checked from "./images/checked.png"
-import brac from "./images/bra.png"
+import brac from "./images/belt.png"
 
 function App() {
   const [watch,setWatch] = React.useState(
@@ -25,45 +25,53 @@ function App() {
   )
   return (
     <div className="App">
-    <div className="App_left">
-   <div className="App_left_nav">
-   <div>
-<img width="60px" height="auto" src={brac}/>
-<h4>Bracelt</h4>
-   </div>
-   <div>
-<img width="60px" height="auto" src={brac}/>
-<h4>Bracelt</h4>
-   </div><div>
-<img width="60px" height="auto" src={brac}/>
-<h4>Bracelt</h4>
-   </div>
-
-   </div>
-   <div className="App_left_content"></div>
-    </div>
-    <div className="App_right">
-<h2>configuration de ceinture de montre (Leather)</h2>
-<div className="active_block">
-
-<img width="100px" height="auto" src={leather}/>
-</div>
-<p>sélectionner votre couleur préférée</p>
-<div className="color_block">
- <div className="color_block_text">Red</div>  <div style={{backgroundColor:"red"}} className="color_block_color"></div>
-</div>
-<div className="color_block">
- <div className="color_block_text">Green</div>  <div  style={{backgroundColor:"green"}} className="color_block_color"></div>
-</div>
-<div className="color_block">
- <div className="color_block_text">orange</div>  <div  style={{backgroundColor:"orange"}} className="color_block_color"> <img src={checked} width="35px" height="auto"/></div>
-</div>
-<div className="color_block">
- <div className="color_block_text">Blue</div>  <div  style={{backgroundColor:"blue"}} className="color_block_color"></div>
+<nav>
+   
+  <div className="logo"> Brand. </div>
+  <ul><li>Home</li><li>Services</li><li>Portfolio</li><li>Contact us</li></ul></nav>
+<div className="content">
+  <div className="content_text">
+  <h1>Personaliser</h1>
+<p>Design your custom watch online and create an unique watch that fits your style</p>
+  </div>
+<div className="content_left" >
+<div className="title">Leather texture</div>
+<div className="title_secondary">Grainy</div>
+<div className="type_list">
+  <div style={{backgroundColor:"brown" ,height:"60px",width:"60px"}}  className="type_list_item"></div>
+  <div style={{backgroundColor:"orange" ,height:"60px",width:"60px"}}  className="type_list_item"> <img height="15px" src={checked}/></div>
+ 
 </div>
 
+</div>
+<div className="content_right" >
+<div className="title">Coleur</div>
+<div className="title_secondary">Aqua blue</div>
+<div className="type_list">
+  <div style={{backgroundColor:"#e2c4a7"}} className="type_list_item"> <img height="15px" src={checked}/></div>
+  <div style={{backgroundColor:"#884a26"}}  className="type_list_item"></div>
+  <div style={{backgroundColor:"#161a1c"}}  className="type_list_item"></div>
+  <div  style={{backgroundColor:"#838381"}} className="type_list_item"></div>
+  <div  style={{backgroundColor:"#7c918d"}} className="type_list_item"></div>
+  <div style={{backgroundColor:"#b0182c"}}  className="type_list_item"></div>
+  <div style={{backgroundColor:"#a0b594"}}  className="type_list_item"></div>
+  <div style={{backgroundColor:"#bab0a4"}}  className="type_list_item"></div>
+  <div style={{backgroundColor:"#796f69"}}  className="type_list_item"></div>
+  <div style={{backgroundColor:"#305a68"}}  className="type_list_item"></div>
+  <div style={{backgroundColor:"#fff"}}  className="type_list_item"></div>
 
-    </div>
+  <div style={{backgroundColor:"#64485a"}}  className="type_list_item"></div>
+
+</div>
+</div>
+
+<div  className="content_right_bottom"> 
+<h3>$ 300</h3>
+<button> ADD TO CART </button>
+
+</div>
+</div>
+
     </div>
   );
 }
