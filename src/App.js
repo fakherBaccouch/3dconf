9a92f306b592c928d973e574b2e497be2a5dbc5e
mohamed.b <bakouchfakher@gmail.com>
2021-11-27 23:28:09 +0100

    lol

--- src/App.js
+++ src/App.js
@@ -19,13 +19,13 @@ function App() {
       matieres: [
         {
           matName: "Leather",
-          textures: ["Flat Grain", "Pebbled","Embossed"],
-          colors: ['#161a1c', 'red' ,'#7c918d', '#bab0a4']
+          textures: ["Flat Grain", "Pebbled", "Embossed"],
+          colors: ['#161a1c', 'red', '#7c918d', '#bab0a4']
         },
         {
           matName: "Metal",
           textures: [],
-          colors: ['#161a1c', '#838381' ,'#7c918d', '#b0182c', '#bab0a4']
+          colors: ['#161a1c', '#838381', '#7c918d', '#b0182c', '#bab0a4']
         },
       ],
     },
@@ -45,7 +45,7 @@ function App() {
         {
           matName: "marker",
           textures: [],
-          colors: ['#161a1c', '#838381' ,'#7c918d', '#b0182c', '#bab0a4'],
+          colors: ['#161a1c', '#838381', '#7c918d', '#b0182c', '#bab0a4'],
         },
         {
           matName: "hand",
@@ -62,11 +62,11 @@ function App() {
   ]);
   const [activePiece, setActivePiece] = React.useState(0);
   const [activeMat, setActiveMat] = React.useState(0);
-const setInit = (i,l)=>{
-  setActiveMat(0);
-  setActivePiece(i)
-  setLeft(l)
-}
+  const setInit = (i, l) => {
+    setActiveMat(0);
+    setActivePiece(i)
+    setLeft(l)
+  }
   return (
     <div className="App">
       <nav>
@@ -78,88 +78,77 @@ const setInit = (i,l)=>{
           <li>Contact us</li>
         </ul>
       </nav>
-      <div className="content"> 
-     
+      <div className="content">
+
         <div className="ligne2" >
-        <div className="content_left">
-          <div className="type_list">
-          {watch[activePiece].matieres.map((m, i) => {
-            return (
-              <div
-                style={{ color: activeMat === i ? "orange" : null ,cursor:"pointer"}}
-                onClick={() => setActiveMat(i)}
-                className="type_list_texture"
-              >
-                {
-                  
-                }
-               <div className="flex-c-c"> <img width='50px' height='auto' src={m.matName === "hand"? hand : m.matName === "dial"? dial:m.matName === "marker" ? marker:m.matName === "Metal" ? metal : m.matName === "Leather"?leather:case_Metal}  /> {m.matName}{" "} </div>
-              
-              
-              </div>
-            );
-          })}
-          </div>
-        </div>
-        <div className="content_middle">
-        <div className="bottom-navigation-responsive ">
-  <div  onClick={()=>setInit(0,10)} className="nav-item " style={{color:"white !important"}} > Bracelet</div>
-  <div onClick={()=>setInit(1,125)} className="nav-item" style={{color:activePiece === 1 ? "white !important":null}} >Cirlce</div>
-  <div onClick={()=>setInit(2,230)} className="nav-item" style={{color:activePiece === 2 ? "black !important":"white !important"}} >Dial</div>
-  <span style={{left:left}} className="nav-indicator"><div style={{height:"8px",width:'8px',borderRadius:"50%",backgroundColor:"white"}}></div></span>
-</div>
-        </div>
-        <div className="content_right">
-          <div>
-            {watch[activePiece].matieres[activeMat].colors.length > 0 && <div className="title">Coleurs</div>}
-            {/* <div className="title_secondary">Aqua blue</div> */}
+          <div className="content_left">
             <div className="type_list">
-              {watch[activePiece].matieres[activeMat].colors && watch[activePiece].matieres[activeMat].colors.map((c) => {
+              {watch[activePiece].matieres.map((m, i) => {
                 return (
                   <div
-                    style={{ backgroundColor: c }}
-                    className="type_list_item"
-                  ></div>
-                );
-              })}
-            </div>
-          </div>
-          <div >
-          {watch[activePiece].matieres[activeMat].textures.length > 0 && <div className="title">Textures</div>}
-            {/* <div className="title_secondary">Aqua blue</div> */}
-            <div style={{marginLeft:"25px"}} className="type_list">
-              {watch[activePiece].matieres[activeMat].textures?.map((t) => {
-                return (
-                  <div className="type_list_texture">
-                    <h4 style={{ color: "gray" }}>{t}</h4>
+                    style={{ color: activeMat === i ? "orange" : null, cursor: "pointer" }}
+                    onClick={() => setActiveMat(i)}
+                    className="type_list_texture"
+                  >
+
+                    <div className="flex-c-c"> <img width='50px' height='auto' src={m.matName === "hand" ? hand : m.matName === "dial" ? dial : m.matName === "marker" ? marker : m.matName === "Metal" ? metal : m.matName === "Leather" ? leather : case_Metal} /> {m.matName}{" "} </div>
                   </div>
                 );
               })}
             </div>
           </div>
-        </div>
+          <div className="content_middle">
+            <div className="bottom-navigation-responsive ">
+              <div onClick={() => setInit(0, 10)} className="nav-item " style={{ color: "white !important" }} > Bracelet</div>
+              <div onClick={() => setInit(1, 125)} className="nav-item" style={{ color: activePiece === 1 ? "white !important" : null }} >Cirlce</div>
+              <div onClick={() => setInit(2, 230)} className="nav-item" style={{ color: activePiece === 2 ? "black !important" : "white !important" }} >Dial</div>
+              <span style={{ left: left }} className="nav-indicator"><div style={{ height: "8px", width: '8px', borderRadius: "50%", backgroundColor: "white" }}></div></span>
+            </div>
+          </div>
+          <div className="content_right">
+            <div>
+              {watch[activePiece].matieres[activeMat].colors.length > 0 && <div className="title">Coleurs</div>}
+              {/* <div className="title_secondary">Aqua blue</div> */}
+              <div className="type_list">
+                {watch[activePiece].matieres[activeMat].colors && watch[activePiece].matieres[activeMat].colors.map((c) => {
+                  return (
+                    <div
+                      style={{ backgroundColor: c }}
+                      className="type_list_item"
+                    ></div>
+                  );
+                })}
+              </div>
+            </div>
+            <div >
+              {watch[activePiece].matieres[activeMat].textures.length > 0 && <div className="title">Textures</div>}
+              {/* <div className="title_secondary">Aqua blue</div> */}
+              <div style={{ marginLeft: "25px" }} className="type_list">
+                {watch[activePiece].matieres[activeMat].textures?.map((t) => {
+                  return (
+                    <div className="type_list_texture">
+                      <h4 style={{ color: "gray" }}>{t}</h4>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          </div>
         </div>
         <div className="ligne3" >
-
-        
-        <div className="content_bottom">
-        <div className="bottom-navigation">
-  <div  onClick={()=>setInit(0,60)} className="nav-item " style={{color:"white !important"}} > {watch[0].piece}</div>
-  <div onClick={()=>setInit(1,240)} className="nav-item" style={{color:activePiece === 1 ? "white !important":null}} >{watch[1].piece}</div>
-  <div onClick={()=>setInit(2,420)} className="nav-item" style={{color:activePiece === 2 ? "black !important":"white !important"}} >{watch[2].piece}</div>
-  <span style={{left:left}} className="nav-indicator"><div style={{height:"15px",width:'15px',borderRadius:"50%",backgroundColor:"white"}}></div></span>
-</div>
-
-
-
+          <div className="content_bottom">
+            <div className="bottom-navigation">
+              <div onClick={() => setInit(0, 60)} className="nav-item " style={{ color: "white !important" }} > {watch[0].piece}</div>
+              <div onClick={() => setInit(1, 240)} className="nav-item" style={{ color: activePiece === 1 ? "white !important" : null }} >{watch[1].piece}</div>
+              <div onClick={() => setInit(2, 420)} className="nav-item" style={{ color: activePiece === 2 ? "black !important" : "white !important" }} >{watch[2].piece}</div>
+              <span style={{ left: left }} className="nav-indicator"><div style={{ height: "15px", width: '15px', borderRadius: "50%", backgroundColor: "white" }}></div></span>
+            </div>
+          </div>
+          <div className="content_right_bottom">
+            <h3> £300.00</h3>
+            <button> ADD TO CART </button>
+          </div>
         </div>
-        
-        <div className="content_right_bottom">
-          <h3> £300.00</h3>
-          <button> ADD TO CART </button>
-        </div>
-        </div>
-        
       </div>
     </div>
   );
@@ -167,17 +156,3 @@ const setInit = (i,l)=>{
 
 export default App;
 
-{
-  /* <div style={{backgroundColor:"#884a26"}}  className="type_list_item"></div>
-<div style={{backgroundColor:"#161a1c"}}  className="type_list_item"></div>
-<div  style={{backgroundColor:"#838381"}} className="type_list_item"></div>
-<div  style={{backgroundColor:"#7c918d"}} className="type_list_item"></div>
-<div style={{backgroundColor:"#b0182c"}}  className="type_list_item"></div>
-<div style={{backgroundColor:"#a0b594"}}  className="type_list_item"></div>
-<div style={{backgroundColor:"#bab0a4"}}  className="type_list_item"></div>
-<div style={{backgroundColor:"#796f69"}}  className="type_list_item"></div>
-<div style={{backgroundColor:"#305a68"}}  className="type_list_item"></div>
-<div style={{backgroundColor:"#fff"}}  className="type_list_item"></div>
-
-<div style={{backgroundColor:"#64485a"}}  className="type_list_item"></div> */
-}
